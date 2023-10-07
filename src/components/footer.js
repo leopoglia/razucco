12fc6c4805c6859ae6cdd834c855730463f2243e
leopoglia <leonardohp2004@gmail.com>
2023-10-07 20:12:52 -0300

    feat: logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="md:flex md:items-center md:justify-between">
                     <a href="https://flowbite.com/" className="flex items-center md-4 md:md-0 justify-center">
-                        <img className="w-16 rounded-xl" src="/imgs/icone.png" alt="" />
+                        <img className="w-10 rounded-lg" src="/imgs/logo.png" alt="" />
                     </a>
                     <ul className="flex flex-wrap items-center md-6 mt-4 md:mt-0 text-sm font-medium text-gray-900 md:md-0 dark:text-gray-900 justify-center">
                         <li>
--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ export default function Header() {
 
   return (
     <header className="header w-full flex items-center justify-center p-8">
-      <img className="w-16 rounded-xl" src="/imgs/icone.png" alt="" />
+      <img className="w-16 rounded-lg p-1 bg-black" src="/imgs/logo.png" alt="" />
     </header>
   );
 }
